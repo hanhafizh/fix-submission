8cb1b988cc0feaf2507513a5fafa50c272b80663
hanhafizh <farhanhafizh770@gmail.com>
2023-06-09 00:25:46 +0700

    Lazy Loading *fix image responsive sharp.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,29 @@
-import 'regenerator-runtime';
-import '../styles/style.css';
-import '../styles/responsive.css';
-import 'lazysizes';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import App from './views/app';
-import swRegister from './utils/sw-register';
+import "regenerator-runtime";
+import "../styles/style.css";
+import "../styles/responsive.css";
+import App from "./views/app";
+import swRegister from "./utils/sw-register";
 
 const app = new App({
-  button: document.querySelector('#hamburgerButton'),
-  drawer: document.querySelector('#navigationDrawer'),
-  content: document.querySelector('#mainContent'),
+  button: document.querySelector("#hamburgerButton"),
+  drawer: document.querySelector("#navigationDrawer"),
+  content: document.querySelector("#mainContent"),
 });
 
-const skipLink = document.querySelector('.skip-link');
-const mainContent = document.querySelector('#mainContent');
+const skipLink = document.querySelector(".skip-link");
+const mainContent = document.querySelector("#mainContent");
 
-skipLink.addEventListener('click', (event) => {
+skipLink.addEventListener("click", (event) => {
   event.preventDefault();
-  mainContent.scrollIntoView({ behavior: 'smooth' });
+  mainContent.scrollIntoView({ behavior: "smooth" });
   skipLink.blur();
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
   swRegister();
 });
